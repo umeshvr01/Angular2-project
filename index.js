b7cc3b7eb02917717c91188b48f46ae5d1542e90
Umesh Verma <umesh.mr.72@gmail.com>
2019-10-28 21:44:07 +0530

    server-setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path = require('path');
+
+
+
+mongoose.connect(config.uri, {useUnifiedTopology: true, useNewUrlParser: true}, (err) => {
+    if (err)
+        console.log('Could NOT connect to database:', err);
+
+    console.log('Connected to database:' + config.db);
+});
+
+app.use(express.static(__dirname + '/client/dist/client'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+  })
+  
+  app.listen(8080, () => {
+      console.log('Listening on port 8080');
+  });
